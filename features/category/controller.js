645f4fbf10e1358039d2a7de936ab1c5e916bae3
Romil Sudani <romil@example.com>
2024-06-25 17:03:18 +0530

    add User-Agent in headers

--- features/category/controller.js
+++ features/category/controller.js
@@ -1,6 +1,5 @@
 import { apiResponse } from "../../helper/apiResponse.js";
 import { StatusCodes } from "http-status-codes";
-import axios from 'axios';
 import cacheService from "../cache/service.js";
 import categoryService from "./service.js";
 import cacheTTL from "../cache/constants.js";
@@ -9,54 +8,34 @@ import Tournament from "./tournamentSchema.js";
 const getAllTournamentsByCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
-    console.log("id...", id);
 
-    // const key = cacheService.getCacheKey(req);
+    const key = cacheService.getCacheKey(req);
 
-    // let data = cacheService.getCache(key);
-    // console.log("data from cache", data);
+    let data = cacheService.getCache(key);
 
-    // if (!data) {
+    if (!data) {
       const headers = {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
       };
-    //   console.log("Request headers:", headers);
+      data = await categoryService.getAllTournamentsByCategory(id, headers);
 
-    //   data = await categoryService.getAllTournamentsByCategory(id, headers);
-    //   console.log("data from service", data);
+      cacheService.setCache(key, data, cacheTTL.ONE_DAY);
+    }
 
-    //   cacheService.setCache(key, data, cacheTTL.ONE_DAY);
-    // }
+    const tournamentEntry = new Tournament({ data });
+    await tournamentEntry.save();
 
-    // const tournamentEntry = new Tournament({ data });
-    // await tournamentEntry.save();
-
-    const url = `https://www.sofascore.com/api/v1/category/${id}/unique-tournaments`;
-    const response = await axios.get(url, { headers });
-    console.log("response------------", response)
-    // return response.data;
 
     return apiResponse({
       res,
-      data: response.data,
+      data: data,
       status: true,
       message: "unique tournament by category fetched successfully",
       statusCode: StatusCodes.OK,
     });
   } catch (error) {
-    console.log("Error occurred:", error);
-
-    if (error.response) {
-      console.log("Response data:", error.response.data);
-      console.log("Response status:", error.response.status);
-      console.log("Response headers:", error.response.headers);
-    } else if (error.request) {
-      console.log("Request data:", error.request);
-    } else {
-      console.log("Error message:", error.message);
-    }
-
     next(error);
   }
 };
--- features/category/service.js
+++ features/category/service.js
@@ -1,9 +1,15 @@
+import axiosInstance from "../../config/axios.config.js";
 import axios from 'axios';
 
 const getAllTournamentsByCategory = async (id, headers) => {
+  // const { data } = await axiosInstance.get(
+  //   `/api/v1/category/${id}/unique-tournaments`
+  // );
+  // return data.groups?.[0]?.uniqueTournaments ?? [];
+
   const url = `https://www.sofascore.com/api/v1/category/${id}/unique-tournaments`;
   const response = await axios.get(url, { headers });
-  console.log("response", response)
+  console.log("response", response);
   return response.data;
 };
 
